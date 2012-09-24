f38c9517bd142b7fed1a258a924e9525f77b990b
Sangjung Derick Woo <again4you@gmail.com>
2012-09-24 15:13:46 +0900

    Modify install script

--- build_env/init_script/install_java6.sh
+++ build_env/init_script/install_java6.sh
@@ -4,9 +4,10 @@
 cd ~/Downloads
 mkdir tmp
 cd tmp
-wget https://raw.github.com/flexiondotorg/oab-java6/master/oab-java6.sh -O oab-java6.sh
-chmod +x oab-java6.sh
-sudo ./oab-java6.sh
+#wget https://raw.github.com/flexiondotorg/oab-java6/master/oab-java6.sh -O oab-java6.sh
+wget https://raw.github.com/flexiondotorg/oab-java6/master/oab-java.sh
+chmod +x oab-java.sh
+sudo ./oab-java.sh
 sudo apt-get install sun-java6-jdk -y
 
-
+sudo update-alternatives --config java
