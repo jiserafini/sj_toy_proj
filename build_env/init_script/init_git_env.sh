ce4b011226b7b71a77d84343f85fcf9f74213459
Sangjung Woo <again4you@gmail.com>
2013-04-20 11:20:02 +0900

    Modify init git script

--- build_env/init_script/init_git_env.sh
+++ build_env/init_script/init_git_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git config --global user.name "Sangjung Derick Woo"
+git config --global user.name "Sangjung Woo"
 git config --global user.email again4you@gmail.com
 git config --global color.ui "auto"
 git config --global core.editor "vim"
